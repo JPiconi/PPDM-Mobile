7bd5b046c6b732bc8e735f724cd130d7138a9620
Júlia Piconi <163847289+JPiconi@users.noreply.github.com>
2025-05-07 07:43:23 -0300

    uai

--- JubisFirstApp/App.js
+++ JubisFirstApp/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} />
-      <TextInput style={styles.input2} />
+      {/* {[GRUPO, {LINHA}]}*/}
+      <Text style={styles.text}>WELCOME FELLOW PARTNER!!!!</Text>
+      <Text style={{ color: "#c2b5a7", fontSize: 17 }}>
+        MAKE YOURSELF AT HOME!!!
+      </Text>
     </View>
   );
 }
@@ -13,28 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#454545",
+    backgroundColor: "#4c563f",
     alignItems: "center",
     justifyContent: "center",
   },
-  input: {
-    borderWidth: 0,
-    borderBottomWidth: 1,
-    borderColor: "#fff",
-    padding: 5,
-    width: 70,
-  },
-  input2: {
-    borderWidth: 0,
-    borderColor: "#fff",
-    textAlign: "center",
-    marginTop: 20,
-    width: 70,
-    padding: 5,
-    borderRadius: 15,
-    shadowColor: "blue",
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
+  text: {
+    color: "#fff",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 10,
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
--- JubisFirstApp/exemplo1.js
+++ JubisFirstApp/exemplo1.js
@@ -1,14 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* {[GRUPO, {LINHA}]}*/}
-      <Text style={styles.text}>WELCOME FELLOW PARTNER!!!!</Text>
-      <Text style={{ color: "#c2b5a7", fontSize: 17 }}>
-        MAKE YOURSELF AT HOME!!!
-      </Text>
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input2} />
     </View>
   );
 }
@@ -16,16 +13,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#4c563f",
+    backgroundColor: "#454545",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
-    textShadowColor: "rgba(0, 0, 0, 0.5)",
-    textShadowOffset: { width: 2, height: 2 },
-    textShadowRadius: 10,
-    fontSize: 20,
-    fontWeight: "bold",
+  input: {
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    borderColor: "#fff",
+    padding: 5,
+    width: 70,
+  },
+  input2: {
+    borderWidth: 0,
+    borderColor: "#fff",
+    textAlign: "center",
+    marginTop: 20,
+    width: 70,
+    padding: 5,
+    borderRadius: 15,
+    shadowColor: "blue",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
   },
 });
